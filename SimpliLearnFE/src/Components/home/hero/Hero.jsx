4e1ce080ce56017e871d74b4b5708ed300a287e5
jugof <jugisoff@gmail.com>
2024-08-22 03:18:13 +0530

    v0.2

--- SimpliLearnFE/src/Components/home/hero/Hero.jsx
+++ SimpliLearnFE/src/Components/home/hero/Hero.jsx
@@ -1,8 +1,16 @@
-import React from "react"
-import Heading from "../../common/heading/Heading"
-import "./Hero.css"
+import React from "react";
+import Heading from "../../common/heading/Heading";
+import "./Hero.css";
+import { Redirect } from "react-router-dom/cjs/react-router-dom";
 
 const Hero = () => {
+
+  const navigate = Redirect;
+
+  const handleClick = () => {
+    navigate("/register");
+  }
+
   return (
     <>
       <section className='hero'>
@@ -11,7 +19,7 @@ const Hero = () => {
             <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
             <div className='button'>
-              <button className='primary-btn'>
+              <button className='primary-btn' onClick={handleClick}>
                 GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
               </button>
               <button>
