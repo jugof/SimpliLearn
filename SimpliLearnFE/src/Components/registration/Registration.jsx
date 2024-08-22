ab613298dd2ba55b2c128e7823989f1dd9f45665
Bhagyashri Barhate <bhagyashridipakbarhate@gmail.com>
2024-08-22 21:31:13 +0530

    v0.3

--- SimpliLearnFE/src/Components/registration/Registration.jsx
+++ SimpliLearnFE/src/Components/registration/Registration.jsx
@@ -4,105 +4,53 @@ import {
   MDBContainer,
   MDBCard,
   MDBCardBody,
-  MDBCardImage,
   MDBRow,
   MDBCol,
   MDBInput,
   MDBRadio,
-  //MDBSelect
 }
 from 'mdb-react-ui-kit';
 
 function Registration() {
   return (
-    <MDBContainer fluid className='bg-dark'>
+    <MDBContainer fluid className='d-flex justify-content-center align-items-center min-vh-100' style={{ 
+      backgroundImage: `url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp')`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backdropFilter: 'blur(8px)'
+    }}>
+      <MDBCard className='my-4 w-50' style={{ backdropFilter: 'blur(8px)', backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
+        <MDBCardBody className='text-black d-flex flex-column justify-content-center align-items-center'>
+          <h3 className="mb-5 text-uppercase fw-bold text-center"> Registration Form</h3>
 
-      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-        <MDBCol>
+          <MDBRow className='w-100'>
+            <MDBCol md='12' className="d-flex flex-column align-items-center">
+              <MDBInput wrapperClass='mb-4' label='User name' size='lg' id='form1' type='text' />
+              <MDBInput wrapperClass='mb-4' label='Password' size='lg' id='form2' type='text' />
+              <MDBInput wrapperClass='mb-4' label='Full Name' size='lg' id='form3' type='text' />
+              <MDBInput wrapperClass='mb-4' label='Email Id' size='lg' id='form4' type='text' />
+              {/* <MDBInput wrapperClass='mb-4' label='Is Admin' size='lg' id='form5' type='text' /> */}
 
-          <MDBCard className='my-4'>
 
-            <MDBRow className='g-0'>
+              <div className="mb-4 w-100">
+                <label className="form-label">Account Type:</label>
+                <select className="form-select" id="form7" aria-label="Account Type">
+                  <option value="" disabled selected>Select account type</option>
+                  <option value="student">Student</option>
+                  <option value="instructor">Instructor</option>
+                  <option value="admin">Admin</option>
+                </select>
+              </div>
+            </MDBCol>
+          </MDBRow>
 
-              <MDBCol md='6' className="d-none d-md-block">
-                <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp' alt="Sample photo" className="rounded-start" fluid/>
-              </MDBCol>
+          <div className="d-flex justify-content-end pt-3 w-100">
+            <MDBBtn color='light' size='lg'>Reset all</MDBBtn>
+            <MDBBtn className='ms-2' color='warning' size='lg'>Submit form</MDBBtn>
+          </div>
 
-              <MDBCol md='6'>
-
-                <MDBCardBody className='text-black d-flex flex-column justify-content-center'>
-                  <h3 className="mb-5 text-uppercase fw-bold">Student registration form</h3>
-
-                  <MDBRow>
-
-                    <MDBCol md='6'>
-                      <MDBInput wrapperClass='mb-4' label='First Name' size='lg' id='form1' type='text'/>
-                    </MDBCol>
-
-                    <MDBCol md='6'>
-                      <MDBInput wrapperClass='mb-4' label='Last Name' size='lg' id='form2' type='text'/>
-                    </MDBCol>
-
-                  </MDBRow>
-
-                  <MDBInput wrapperClass='mb-4' label='Birthday' size='lg' id='form3' type='text'/>
-
-                  <div className='d-md-flex ustify-content-start align-items-center mb-4'>
-                    <h6 class="fw-bold mb-0 me-4">Gender: </h6>
-                    <MDBRadio name='inlineRadio' id='inlineRadio1' value='option1' label='Female' inline />
-                    <MDBRadio name='inlineRadio' id='inlineRadio2' value='option2' label='Male' inline />
-                    <MDBRadio name='inlineRadio' id='inlineRadio3' value='option3' label='Other' inline />
-                  </div>
-
-                  <MDBRow>
-
-                    <MDBCol md='6'>
-                      {/* <MDBSelect
-                        className='mb-4'
-                        size='lg'
-                        data={[
-                          { text: 'State', value: 1 },
-                          { text: 'Option 1', value: 2 },
-                          { text: 'Option 2', value: 3 },
-                          { text: 'Option 3', value: 4 }
-                        ]}
-                        /> */}
-                    </MDBCol>
-
-                    <MDBCol md='6'>
-                      {/* <MDBSelect
-                        className='mb-4'
-                        size='lg'
-                        data={[
-                          { text: 'City', value: 1 },
-                          { text: 'Option 1', value: 2 },
-                          { text: 'Option 2', value: 3 },
-                          { text: 'Option 3', value: 4 }
-                        ]}
-                        /> */}
-                    </MDBCol>
-
-                  </MDBRow>
-
-                  <MDBInput wrapperClass='mb-4' label='Pincode' size='lg' id='form4' type='text'/>
-                  <MDBInput wrapperClass='mb-4' label='Course' size='lg' id='form5' type='text'/>
-                  <MDBInput wrapperClass='mb-4' label='Email ID' size='lg' id='form6' type='text'/>
-
-                  <div className="d-flex justify-content-end pt-3">
-                    <MDBBtn color='light' size='lg'>Reset all</MDBBtn>
-                    <MDBBtn className='ms-2' color='warning' size='lg'>Submit form</MDBBtn>
-                  </div>
-
-                </MDBCardBody>
-
-              </MDBCol>
-            </MDBRow>
-
-          </MDBCard>
-
-        </MDBCol>
-      </MDBRow>
-
+        </MDBCardBody>
+      </MDBCard>
     </MDBContainer>
   );
 }
